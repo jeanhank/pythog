953ada28d110621dde2203d5d7ca96283327c4c4
jeanhank <jeanhank@gmail.com>
2016-12-29 19:09:36 -0800

    fix init

--- pythog.py
+++ pythog.py
@@ -16,11 +16,3 @@ class Discotrek:
 	user_agent = 'DiscotrekApp/0.1'
 
 	client = discogs_client.Client(user_agent)
-
-	def __init__(self):
-		self.consumer_key = consumer_key
-		self.consumer_secret = consumer_secret
-		self.request_token_URL = request_token_URL
-		self.authorize_URL = authorize_URL
-		self.access_token_URL = access_token_URL
-		self.client = client
