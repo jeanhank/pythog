98dcdb4508388adddc8613466007ee03a4ff9c7a
jeanhank <jeanhank@gmail.com>
2016-12-29 19:16:47 -0800

    add user token auth

--- pythog.py
+++ pythog.py
@@ -13,6 +13,9 @@ class Discotrek:
 	authorize_URL = 'https://www.discogs.com/oauth/authorize'
 	access_token_URL = 'https://api.discogs.com/oauth/access_token'
 
+	#temporary token just to see. need to implement OAuth
+	my_user_token = 'FUVJEoiYuwgeuLGgPUEqvlYiGCEnyymAHtKbtSrw'
+
 	user_agent = 'DiscotrekApp/0.1'
 
-	client = discogs_client.Client(user_agent)
+	client = discogs_client.Client(user_agent, user_token = my_user_token)
